1df31715ba5285264aec6f846c6e4c1856a1a2fb
TOMMY-parrot <yassinguellouz@gmail.com>
2023-06-15 08:18:44 -0700

    e

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
+/**
+ * main - A program print line using puts
+ * Return: Always 0.
+ */
 	int main (void)
 {	
-		puts("Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 		return 0;
 }
