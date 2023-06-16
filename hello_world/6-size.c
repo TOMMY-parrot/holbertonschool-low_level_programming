182c5e085219e4a49ea2455df9e4c053922709d0
TOMMY-parrot <yassinguellouz@gmail.com>
2023-06-16 01:58:09 -0700

    e

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,15 @@
 # include (stdio.h)
 /**
- * Description:main - Print the size of various types onthe computer.
+ * Description:main - Print the size of various
+ * types onthe computer.
  * Return: 0 if sucess.
  */
+
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-	printf("size of int: %d bytes\n" , sizeof(int));
-	printf("size of float: %d bytes\n" , sizeof(float));
-	printf("size of double: %d bytes\n" , sizeof(double));
-	printf("size of char %d bytes\n" , sizeof(char));
-	return 0;
+	printf("size of int: %d bytes\n", sizeof(int));
+	printf("size of float: %d bytes\n", sizeof(float));
+	printf("size of double: %d bytes\n", sizeof(double));
+	printf("size of char %d bytes\n", sizeof(char));
+	return (0);
 }
