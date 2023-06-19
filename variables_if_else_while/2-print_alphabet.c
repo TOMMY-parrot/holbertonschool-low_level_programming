e4d0624bd697362110cf4453754681affe851cad
TOMMY-parrot <yassinguellouz@gmail.com>
2023-06-19 03:15:37 -0700

    e

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++){
 		putchar(letter);
+               	}
 		printf("\n");
 
 	return (0);
