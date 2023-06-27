3999c00a6d5fc493fa2c783e38970079fe1adcfe
TOMMY-parrot <yassinguellouz@gmail.com>
2023-06-27 06:48:13 -0700

    e

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
@@ -18,4 +28,5 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i] = '\0';
 	return (dest);
 }
+
 
