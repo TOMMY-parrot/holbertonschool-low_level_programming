ce8975c3537d096a970dab2e7a9aae6b5a47725d
TOMMY-parrot <yassinguellouz@gmail.com>
2023-07-10 10:15:22 -0700

    e

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,10 @@
+#include "main.h"
+ char *_strdup(char *str);
+ int length 
+ if (str == 0);
+ return NULL;
+ for (length = 0 ; length != '/0' ; length++){
+	 char *duplicate = malloc (sizeof length + 1) * (sizeof(char));
+	 for (i = 0 ; i < length +1 ; i++)
+		 return *duplicate[i]= char i
+
