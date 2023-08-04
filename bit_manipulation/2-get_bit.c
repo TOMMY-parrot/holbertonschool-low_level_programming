d27da24529c285ed93f14755773139c29f4ff205
TOMMY-parrot <yassinguellouz@gmail.com>
2023-08-04 08:03:39 -0700

    e

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
- * @index: index of the bit
- *
- * Return: value of the bit
+ * get_bit - bit position.
+ * @n: number.
+ * @index: bits.
+ * Return: position.
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
 
-	if (index > 63)
+	if (sizeof(unsigned long int) * 8 < index)
+	{
 		return (-1);
-
-	bit_val = (n >> index) & 1;
-
-	return (bit_val);
+	}
+	n  >>= index;
+	if (n & 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a given bit to 0
- * @n: pointer to the number to change
- * @index: index of the bit to clear
- *
- * Return: 1 for success, -1 for failure
+ * clear_bit - clear a bit to zero.
+ * @n: number.
+ * @index: move position change.
+ * Return: 1 if all ok.
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-		return (-1);
+	unsigned long int start = 1;
 
-	*n = (~(1UL << index) & *n);
+	if ((sizeof(unsigned long int) * 8) - 1 < index)
+	{
+		return (-1);
+	}
+	start <<= index;
+	*n &= ~start;
+
 	return (1);
 }
