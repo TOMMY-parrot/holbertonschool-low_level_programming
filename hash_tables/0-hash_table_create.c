2ee7f77f99b6149a12547e6d6dca4fe9cd1c300c
TOMMY-parrot <yassinguellouz@gmail.com>
2023-08-11 07:18:30 -0700

    e

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - hash table creation 
+ * Desc: Entry
+ * @size: the size of the hashtable
+ * Return: a pointer to the newly allocated hashtable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (!(table->array))
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	return (table);
+}
